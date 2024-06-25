c85b01a7a9bcc2e7e5ff3344d7830f3820eaf6b4
Joaquin Guerrero <joaquinguerrero256@gmail.com>
2024-06-25 20:58:41 -0300

    se agrego funcion para filtrar moneda por seleccion

--- js/index.js
+++ js/index.js
@@ -5,64 +5,74 @@ document.addEventListener('DOMContentLoaded', () => {
     // Escuchar cambios en el combo box
     const selectMoneda = document.getElementById('moneda');
     selectMoneda.addEventListener('change', () => {
-        actualizarCotizaciones(selectMoneda.value.toLowerCase());
+        actualizarCotizacionEnDOM(selectMoneda.value.toLowerCase());
     });
 });
 
 const api_url = "https://dolarapi.com/v1";
 let cotizaciones = {}; // Diccionario para almacenar las cotizaciones
 
-function consultar_cotizacion() {
+function consultarCotizaciones() {
     let divisas = ["oficial", "blue", "bolsa", "contadoconliqui", "tarjeta", "mayorista", "cripto", "eur", "brl", "clp", "uyu"];
+    let cotizaciones = {};
 
     for (let i = 0; i < divisas.length; i++) {
         let url = "";
         if (!["eur", "brl", "clp", "uyu"].includes(divisas[i])) {
-            url = api_url + "/dolares/" + divisas[i];
+            url = `${api_url}/dolares/${divisas[i]}`;
         } else {
-            url = api_url + "/cotizaciones/" + divisas[i];
+            url = `${api_url}/cotizaciones/${divisas[i]}`;
         }
 
         fetch(url)
             .then(response => {
                 if (!response.ok) {
-                    throw new Error('Error al obtener los datos de la API para ' + divisas[i]);
+                    throw new Error(`Error al obtener los datos de la API para ${divisas[i]}`);
                 }
                 return response.json();
             })
             .then(data => {
-                // Almacenar los datos en el diccionario
                 cotizaciones[divisas[i]] = data;
+                actualizarCotizacionEnDOM(divisas[i], data);
 
-                // Llamar a la función para actualizar las cotizaciones según la selección actual
-                actualizarCotizaciones(document.getElementById('moneda').value.toLowerCase());
             })
             .catch(error => {
                 console.error("ERROR:", error);
             });
     }
+    console.log('cotizaciones', cotizaciones)
 }
 
-function actualizarCotizaciones(monedaSeleccionada) {
-    let divisas = ["oficial", "blue", "bolsa", "contadoconliqui", "tarjeta", "mayorista", "cripto", "eur", "brl", "clp", "uyu"];
+function actualizarCotizacionEnDOM(moneda, data) {
+    const compra = data.compra;
+    const venta = data.venta;
+    const fecha_actualizacion = data.fechaActualizacion;
 
-    for (let i = 0; i < divisas.length; i++) {
-        // Verificar si mostrar esta divisa según la selección del combo box
-        if (monedaSeleccionada === "todas" || monedaSeleccionada === divisas[i]) {
-            const compra = cotizaciones[divisas[i]].compra;
-            const venta = cotizaciones[divisas[i]].venta;
+    let elementoCompra = document.querySelector(`.compra-dolar-${moneda}`);
+    let elementoVenta = document.querySelector(`.venta-dolar-${moneda}`);
+    elementoCompra.textContent = compra;
+    elementoVenta.textContent = venta;
 
-            let elementoCompra = document.querySelector(".compra-dolar-" + divisas[i]);
-            let elementoVenta = document.querySelector(".venta-dolar-" + divisas[i]);
-            
-            if (elementoCompra && elementoVenta) { // Verificar que los elementos existan antes de actualizar
-                elementoCompra.textContent = compra;
-                elementoVenta.textContent = venta;
-            }
+    let elementoFecha = document.getElementById("fecha-actualizada");
+    elementoFecha.textContent = `Datos actualizados al ${fecha_actualizacion}`;
+}
+
+function filtrarCotizaciones() {
+    const seleccion = document.getElementById("moneda").value.toLowerCase();
+    const columnas = document.querySelectorAll(".columna");
+
+    for (let i = 0; i < columnas.length; i++) {
+        const columna = columnas[i];
+        const nombreMoneda = columna.querySelector("h3").textContent.trim().toLowerCase();
+        const mostrar = seleccion === "todas" || nombreMoneda.includes(seleccion);
+
+        if (mostrar) {
+            columna.style.display = "flex";
+        } else {
+            columna.style.display = "none";
         }
     }
 }
 
-function cambiarMoneda(monedaSeleccionada) {
-    actualizarCotizaciones(monedaSeleccionada);
-}
+
+consultarCotizaciones();
