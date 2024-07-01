9f1bc5b8da8dc2a338b7a05decf929429392c666
Agustín Servin <agustinservin199103@gmail.com>
2024-07-01 19:07:57 -0300

    Se agregara la función que elimina favoritos en localstorage y html

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if (favoritos.length > 0) {
             const fecha_actualizacion = new Date(favoritos[0].fecha_actualizacion);
 
-        // FORMATO FECHA
+            // FORMATO FECHA
             const dia = String(fecha_actualizacion.getDate()).padStart(2, '0');
             const mes = String(fecha_actualizacion.getMonth() + 1).padStart(2, '0'); // Los meses son 0-indexados
             const anio = fecha_actualizacion.getFullYear();
@@ -25,23 +25,42 @@ document.addEventListener("DOMContentLoaded", () => {
             const tr = document.createElement('tr');
             const tbody = document.getElementById('tabla-datos');
             tr.classList.add('datos');
+            tr.dataset.index=i;// Asignar el índice del favorito al atributo data-index
             const contenido = ["",favoritos[i].moneda, favoritos[i].compra, favoritos[i].venta, ""]
             console.log(contenido)
                 for(let z=0; z<contenido.length; z++){
                     const td = document.createElement("td")
-                    if (z==1){
+                    if (z===1){
                         td.classList.add("moneda")
                     }
-                    td.textContent=contenido[z]
+                    if (z === contenido.length-1) {
+                        const button= document.createElement('button');
+                        button.classList.add('button-delete');
+                        button.innerHTML='<i class="fa-solid fa-trash"></i>';
+                        button.addEventListener('click', eliminarFavorito);
+                        td.appendChild(button)
+                    } else {
+                        td.textContent = contenido[z];
+                    }  
                     tr.appendChild(td)
-                }
-
-            
-             
+                }             
             tbody.appendChild(tr);
         }
         
     }
+    // Agregar el evento click a los botones 
+
+    function eliminarFavorito(event){
+        const tr= event.target.closest('tr'); //Obtenemos la fila más cercana al botón que dispara el evento
+        const index= tr.dataset.index;
+        favoritos.splice(index,1);//Eliminamos el favorito del array usando el indice
+        localStorage.setItem('favoritos', JSON.stringify(favoritos));//Actualización localstorage
+        tr.remove();
+        document.getElementById('tabla-datos').innerHTML="";
+        mostrarFavoritos();
+    } 
+
+
 
 /*<td><button class="button-delete"><i class="fa-solid fa-trash"></i></button></td>*/
 /*<tbody>
@@ -55,3 +74,5 @@ document.addEventListener("DOMContentLoaded", () => {
 <td class="venta">$1015</td>
 <td><button class="button-delete"><i class="fa-solid fa-trash"></i></button></td>
 </tr>*/
+
+
